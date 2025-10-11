6fa90ee8ab4e66c719470c4d0c8a6b09eabf4a47
yashG <yrg.creation.ai@gmail.com>
2025-10-11 22:23:53 +0530

    pre defined object parameter for component for scalability

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import CinemaSeatBooking from './Components/Cinema-Seat-Booking'
 
 const App = () => {
   return (
-    <div>App
-      <CinemaSeatBooking />
-    </div>
-  )
+      <CinemaSeatBooking 
+           layout = {{
+             rows : 8,
+             seatsPerRow : 12,
+             aislePosition: 5,
+           }}
+           seatTypes = {{
+             regular: {name: "Regular", Price: 150, rows: [0,1,2] },
+             premium: {name: "Premium", Price: 250, rows: [3,4,5] },
+             vip : {name: "VIP", Price: 350, rows: [6,7,8] },
+           }}
+      />
+  );
 }
 
 export default App
--- src/Components/Cinema-Seat-Booking.jsx
+++ src/Components/Cinema-Seat-Booking.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
 
-const CinemaSeatBooking = () => {
+const CinemaSeatBooking = ( {
+    layout = {
+        rows : 8,
+        seatsPerRow : 12,
+        aislePosition: 5,
+    },
+    seatTypes = {
+        regular: {name: "Regular", Price: 150, rows: [0,1,2] },
+        premium: {name: "Premium", Price: 250, rows: [3,4,5] },
+        vip : {name: "VIP", Price: 350, rows: [6,7,8] },
+    },
+    bookedSeat = [],
+    currency = "₹",
+    onBookingComplete = () => {},
+    title = "YXCinemas",
+    subtitle = "we are the best in cinemas.",
+
+}) => {
   return (
     <div>Cinema-Seat-Booking
         {/* title */}
