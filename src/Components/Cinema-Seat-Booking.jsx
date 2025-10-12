c1117a658ab115ee19fbbddb97acc46abb15261c
yashG <yrg.creation.ai@gmail.com>
2025-10-12 22:16:20 +0530

    seats displayed

--- src/Components/Cinema-Seat-Booking.jsx
+++ src/Components/Cinema-Seat-Booking.jsx
@@ -11,22 +11,62 @@ const CinemaSeatBooking = ( {
         premium: {name: "Premium", Price: 250, rows: [3,4,5] },
         vip : {name: "VIP", Price: 350, rows: [6,7,8] },
     },
-    bookedSeat = [],
+    bookedSeats = [],
     currency = "₹",
     onBookingComplete = () => {},
     title = "YX-Cinemas",
     subtitle = "we are the best in cinemas.",
 
 }) => {
+
+  const getSeatType = () => {
+
+  }
+
  // this function will only run when something change in this dependency [] array
   const initializeSeats = useMemo(() => {
      const seats = [];
-     for (let row = 0; row < layout.rows; row++) {
+     for (let row = 0; row < layout.rows; row++) { 
            const seatRow = [];
+           const seatTypeInfo = getSeatType(row);
+
+           for(let seat = 0; seat < layout.seatsPerRow; seat++) {
+                 const seatId = `${String.fromCharCode(65 + row)} ${seat + 1}`;
+
+                 seatRow.push({
+                  id: seatId,
+                  row,
+                  seat,
+                  type: seatTypeInfo?.type || "regular",
+                  price: seatTypeInfo?.price || 150,
+                  color: seatTypeInfo?.color || "blue",
+                  status: bookedSeats.includes(seatId) ? "booked" : "available",
+                  selected: false,
+                 });
+           }
+           seats.push(seatRow);
      }
-  }, [layout, seatTypes, bookedSeat]);
+     return seats;
+
+  }, [layout, seatTypes, bookedSeats]);
 
   const [seats, setSeats] = useState(initializeSeats);
+
+
+  const renderSeatSection = (seatRow, startIndex, endIndex) => {
+    return (
+        <div className="render-seat-d">
+            {seatRow.slice(startIndex, endIndex).map((seat, index) => {
+               return( 
+                <div className="default-seat"> 
+                    {startIndex + index + 1}
+                </div>
+               );
+            })}
+        </div>
+    ); 
+    
+  };
 
   return (
     <div className="container-div">
@@ -44,9 +84,22 @@ const CinemaSeatBooking = ( {
 
         {/* seats map */}
         <div className="seats-map-d">
-           <div className="seats-rows">
+           <div className="seats-container">
                {
-                  
+                  seats.map((row, rowIndex) => {
+                     return (
+                       <div className="seats-rows" key={rowIndex}>
+                          <span>{String.fromCharCode(65 + rowIndex)}</span>
+                          {renderSeatSection(row, 0, layout.aislePosition )}
+                          {/* aisle */}
+                          {renderSeatSection(
+                            row, 
+                            layout.aislePosition,
+                            layout.seatsPerRow
+                          )}
+                       </div>
+                     )
+                  })
                }
            </div>
         </div>
